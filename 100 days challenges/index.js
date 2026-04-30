047cad87ccb6341a48cbc690447488b34d8a92ba
sona mittal <sonamittal285@gmail.com>
2026-04-30 16:08:20 +0530

    O

--- 100 days challenges/index.js
+++ 100 days challenges/index.js
@@ -17,3 +17,18 @@ const normCEn = (s) => {
   return result;
 };
 console.log(normCEn("tour"));
+
+// Manager loves to use AI
+const getAppraisalPercentage = (a, k, b) => {
+  // sort arr
+  a.sort((a, b) => b - a);
+  // kth element
+  let kthElement = a[k - 1];
+  // apply formla
+  let res = (kthElement / b) * 100;
+  return Math.floor(res);
+};
+let a = [10000, 2000, 4000, 5000, 7000];
+let k = 2;
+let b = 40000;
+console.log(getAppraisalPercentage(a, k, b));
